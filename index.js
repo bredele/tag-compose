64a34e75f305887655dce73174bf8af59892df02
bredele <olivier.wietrich@gmail.com>
2018-07-03 17:55:17 -0600

    compose tagged templates at given index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 module.exports = (first, second, index = 0) => {
   const element = first[index]
   const last = second.pop()
-  first.splice(index + 1, 1, first[index + 1] + last)
+  first.splice(index + 1, 1, (first[index + 1] || '') + last)
   second[0] = element + second[0]
   first.splice(index, 1, ...second)
   return first
--- test/tag-compose.js
+++ test/tag-compose.js
@@ -5,7 +5,7 @@
 const test = require('tape')
 const compose = require('..')
 
-test('compose tagged templates finishing with empty string', assert => {
+test('compose tagged templates of same length finishing with empty string', assert => {
   assert.plan(1)
   assert.deepEqual(
     compose(['hello', ''], [' world', '']),
@@ -13,10 +13,18 @@ test('compose tagged templates finishing with empty string', assert => {
   )
 })
 
-test('compose tagged templates finishing with string', assert => {
+test('compose tagged templates of same length finishing with string', assert => {
   assert.plan(1)
   assert.deepEqual(
     compose(['hello', '!'], [' world', '']),
     ['hello world', '!']
+  )
+})
+
+test('compose tagged templates at given index', assert => {
+  assert.plan(1)
+  assert.deepEqual(
+    compose(['hello', ''], ['world', ''], 1),
+    ['hello', 'world', '']
   )
 })
