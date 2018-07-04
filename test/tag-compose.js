074f6ccc6f6ee88c2912f0aa4e4b057f82aeb887
bredele <olivier.wietrich@gmail.com>
2018-07-03 18:03:30 -0600

    compose tagged templates at given index where both finish with string

--- test/tag-compose.js
+++ test/tag-compose.js
@@ -5,7 +5,7 @@
 const test = require('tape')
 const compose = require('..')
 
-test('compose tagged templates of same length finishing with empty string', assert => {
+test('compose tagged templates where first finish with empty string', assert => {
   assert.plan(1)
   assert.deepEqual(
     compose(['hello', ''], [' world', '']),
@@ -13,7 +13,7 @@ test('compose tagged templates of same length finishing with empty string', asse
   )
 })
 
-test('compose tagged templates of same length finishing with string', assert => {
+test('compose tagged templates fin where first finish with string', assert => {
   assert.plan(1)
   assert.deepEqual(
     compose(['hello', '!'], [' world', '']),
@@ -21,10 +21,34 @@ test('compose tagged templates of same length finishing with string', assert => 
   )
 })
 
-test('compose tagged templates at given index', assert => {
+test('compose tagged templates at given index where first finish with string', assert => {
   assert.plan(1)
   assert.deepEqual(
     compose(['hello', ''], ['world', ''], 1),
     ['hello', 'world', '']
+  )
+})
+
+test('compose tagged templates at given index where first finish with string', assert => {
+  assert.plan(1)
+  assert.deepEqual(
+    compose(['hello', 'everybody in the ', '!'], ['world', ''], 1),
+    ['hello', 'everybody in the world', '!']
+  )
+})
+
+test('compose tagged templates at given index where first finish with empty string', assert => {
+  assert.plan(1)
+  assert.deepEqual(
+    compose(['hello', 'everybody in the ', ''], ['world', '!'], 1),
+    ['hello', 'everybody in the world', '!']
+  )
+})
+
+test('compose tagged templates at given index where both finish with string', assert => {
+  assert.plan(1)
+  assert.deepEqual(
+    compose(['hello', 'everybody in the ', 'and the universe'], ['world', '!'], 1),
+    ['hello', 'everybody in the world', 'and the universe!']
   )
 })
